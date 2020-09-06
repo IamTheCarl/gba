234e054fe060a52bd915f245a562ab649be0426a
I am the Carl <jamescarl96@gmail.com>
2020-09-06 01:34:05 -0400

    Project correctly auto-links itself when used in crates taht include it. The examples don't do this correctly, yet.

---
+++ build.rs
@@ -0,0 +1,30 @@
+
+use std::env;
+use std::fs;
+use std::path::PathBuf;
+
+fn main() {
+
+    // We want to re-run this build script if one of these files change.
+    println!("cargo:rerun-if-changed=src/crt0.s");
+    println!("cargo:rerun-if-changed=src/linker.ld");
+
+    // Tell cargo where to find the products of this script.
+    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    println!("cargo:rustc-link-search={}", out.display());
+
+    // We need to provide our linker script to the host application.
+    fs::copy("src/linker.ld", out.join("link.x")).unwrap();
+
+    // We need to also link in our assembly files.
+    cc::Build::new()
+        .compiler("arm-none-eabi-gcc")
+        .archiver("arm-none-eabi-ar")
+        .no_default_flags(true)
+        .warnings_into_errors(true)
+        .flag("-mcpu=arm7tdmi")
+        .out_dir(out)
+        .file("src/crt0.s")
+        .compile("crt0");
+    println!("cargo:rustc-link-static={}", "crt0");
+}
--- examples/hello_magic.rs
+++ examples/hello_magic.rs
@@ -1,13 +1,13 @@
 #![no_std]
-#![feature(start)]
+#![no_main]
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
   loop {}
 }
 
-#[start]
-fn main(_argc: isize, _argv: *const *const u8) -> isize {
+#[no_mangle]
+extern "C" fn main() -> ! {
   unsafe {
     (0x400_0000 as *mut u16).write_volatile(0x0403);
     (0x600_0000 as *mut u16).offset(120 + 80 * 240).write_volatile(0x001F);
